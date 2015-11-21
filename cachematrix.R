7c564d1e0f3cd4959f409e1cc241ee26dd3a92de
Stephanie Roark <steph.roark@gmail.com>
2015-11-21 13:39:14 -0800

    ProgAssign2 changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Function to look for matrix and and cache the inverse of a given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() { 
+                x
+        }
+        setmatrix <- function(solve) {
+                m <<- solve
+        }
+        getmatrix <- function() {
+                m
+        }
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## Function to check for an inverse matrix and compute the inverse of given matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
